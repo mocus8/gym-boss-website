5bd7aec87cd90965733f6c2e079090c3b31eae3b
mocus8 <mocus8@email.com>
2025-11-16 14:11:21 +0300

    sms debug

--- src/registration.php
+++ src/registration.php
@@ -2,14 +2,14 @@
 session_start();
 
 require_once DIR . '/helpers.php';
-require_once DIR . '/secure/captchaVerification.php';
+// require_once DIR . '/secure/captchaVerification.php';
 
 // Получаем данные ИЗ POST
 $login = $_POST["login"];
 $password = $_POST["password"]; 
 $name = $_POST["name"];
 
-requireCaptcha();
+// requireCaptcha();
 
 $cartSessionId = getCartSessionId();
 
