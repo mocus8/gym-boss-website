b8ae52ddc819a1a49845bd9789cf8e993a443d93
mocus8 <mocus8@gmail.com>
2025-12-29 23:32:19 +0300

    fix: страница политики конф-сти через шаблоны, удален лишний блок из контроллера kwork_customers

--- src/pages/privacy.php
+++ src/pages/privacy.php
@@ -1,10 +1,12 @@
 <?php
-$title  = 'Реализация проекта - Gym Boss';
-$canonical = $baseUrl . '/kwork_customers';
+// Контроллер страницы с политикой конф-сти
+
+$title  = 'Политика конфиденциальности - Gym Boss';
+$canonical = $baseUrl . '/privacy';
 
 // Через буфер записываем в переменную контент страницы
 ob_start();
-require __DIR__ . '/../templates/pages/kwork_customers.php';
+require __DIR__ . '/../templates/pages/privacy.php';
 $content = ob_get_clean();
 
 // И подключаем главный шаблон сайта
