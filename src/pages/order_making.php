2306c0273c23b2334f948e1b79df9aaaaf7e7ac2
mocus8 <mocus8@gmail.com>
2025-12-29 18:58:38 +0300

    fix: исправлен порядок подключения скриптов карт

--- src/pages/order_making.php
+++ src/pages/order_making.php
@@ -2,7 +2,7 @@
 // Контроллер страницы оформления заказа
 
 // Если нечего нет в корзине в момент оформления - перекидываем на главную
-require_once __DIR__ . '/src/getCartInfo.php';
+require_once __DIR__ . '/../getCartInfo.php';
 if ($cartCount === 0) {
     header('Location: /');
     exit;
@@ -10,7 +10,12 @@ if ($cartCount === 0) {
 
 $title  = 'Оформление заказа - Gym Boss';
 $robots = 'noindex,nofollow';
-$pageScripts = ['/js/maps.js', '/js/order-making.js'];
+$pageScripts = [
+    'https://cdn.jsdelivr.net/npm/@dadata/suggestions@25.4.1/dist/suggestions.min.js',
+    'https://api-maps.yandex.ru/2.1/?apikey=' . getenv('YANDEX_MAPS_KEY') . '&lang=ru_RU&load=package.full',
+    '/js/maps.js',
+    '/js/order-making.js'
+];
 
 // Через буфер записываем в переменную контент страницы
 ob_start();
