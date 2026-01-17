17e65c7dc6a0fef5702b40c9b67f502c7788bf96
mocus8 <mocus8@gmail.com>
2026-01-17 15:03:57 +0300

    feat: сделан api.js с функциями для отправки запросов с фронта

---
+++ js/product/api.js
@@ -0,0 +1,53 @@
+// Файл для взаимодействия с api-контроллером: отправкой запросов и получения ответов.
+// Всегда возвращается объект с полями: { success: true, data: { items, count, total } }
+
+// Импортируем общую функция для взаимодвействия с api
+import { requestApi } from "../utils.js";
+
+const PRODUCT_BASE_URL = "/api/products/";
+
+// Оборачиваем общую функцию (добавляем базовый путь)
+async function requestProduct(path, options = {}) {
+    return requestApi(PRODUCT_BASE_URL, path, options);
+}
+
+// Функция для получения каталога
+// Отправляет запрос GET /api/products
+export function getCatalog() {
+    return requestProduct("", {
+        method: "GET",
+    });
+}
+
+// Функция для получения товара по slug
+// Отправляет запрос GET /api/products/{slug}
+export function getBySlug(slug) {
+    // Защищаем от пробелов и странных символов в slug.
+    const safeSlug = encodeURIComponent(slug);
+
+    return requestProduct(safeSlug, {
+        method: "GET",
+    });
+}
+
+// Функция для получения каталога
+// Отправляет запрос GET /api/products/search?q={query}
+export function searchProducts(query) {
+    let q = query.trim();
+
+    if (!q) {
+        return Promise.resolve([]); // сразу «успешный» промис с пустым массивом
+    }
+
+    // Ограничиваем длину строки
+    if (q.length > 150) {
+        q = q.slice(0, 150);
+    }
+
+    // URLSearchParams правильно собирает query‑строку и кодирует спецсимволы
+    q = new URLSearchParams({ q }).toString();
+
+    return requestProduct(`search?q=${q}`, {
+        method: "GET",
+    });
+}
