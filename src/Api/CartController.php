5fac801fff07cc8ee19ae77978971c7bca1fee20
mocus8 <mocus8@gmail.com>
2026-01-19 21:28:23 +0300

    fix: убрана лишняя запятая

--- src/Api/CartController.php
+++ src/Api/CartController.php
@@ -53,7 +53,7 @@ class CartController {
         return [
             'items' => $items,
             'count' => $count,
-            'total' => $total,
+            'total' => $total
         ];
     }
 
