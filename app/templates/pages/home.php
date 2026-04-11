d6deb1ed0e8807493d2376b0a4bba2ce23d1a03e
mocus8 <mocus8@gmail.com>
2026-04-11 10:07:21 +0300

    refactor: переделана верстка для content-шаблона каталога (home.php)

--- app/templates/pages/home.php
+++ app/templates/pages/home.php
@@ -1,30 +1,34 @@
 <!-- Контент главной страницы -->
 
 <div class="catalog">
-<?php foreach ($catalog as $categoryData) { ?>
-    <div class="category_row">
-        <div class="category_name">
-            <?= htmlspecialchars($categoryData['category']['name'], ENT_QUOTES, 'UTF-8') ?>
-        </div>
-        <div class="products_main">
-            <?php foreach ($categoryData['products'] as $productData) { ?>
-                <a href="/products/<?= htmlspecialchars($productData['slug'], ENT_QUOTES, 'UTF-8') ?>">
-                    <div class="product">
-                        <div class="product_click">
-                            <img class="product_img_1"
-                                 src="<?= htmlspecialchars($productData['image_path'], ENT_QUOTES, 'UTF-8') ?>"
-                                 alt="<?= htmlspecialchars($productData['name'], ENT_QUOTES, 'UTF-8') ?>">
-                            <div class="product_name_1">
-                                <?= htmlspecialchars($productData['name'], ENT_QUOTES, 'UTF-8') ?>
-                            </div>
-                            <div class="product_price_1">
-                                <?= htmlspecialchars(formatPrice((float)$productData['price']), ENT_QUOTES, 'UTF-8') ?> ₽
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            <?php } ?>
-        </div>
-    </div>
-<?php } ?>
+    <?php foreach ($catalog as $categoryData) { ?>
+        <section class="category_row">
+            <h2 class="category_name">
+                <?= htmlspecialchars($categoryData['category']['name'], ENT_QUOTES, 'UTF-8') ?>
+            </h2>
+            
+            <ul class="products_main">
+                <?php foreach ($categoryData['products'] as $productData) { ?>
+                    <li>
+                        <article>
+                            <a class="product" href="/products/<?= htmlspecialchars($productData['slug'], ENT_QUOTES, 'UTF-8') ?>">
+                                <img class="product_img_1"
+                                    src="<?= htmlspecialchars($productData['image_path'], ENT_QUOTES, 'UTF-8') ?>"
+                                    alt="<?= htmlspecialchars($productData['name'], ENT_QUOTES, 'UTF-8') ?>"
+                                >
+
+                                <h3 class="product_name_1">
+                                    <?= htmlspecialchars($productData['name'], ENT_QUOTES, 'UTF-8') ?>
+                                </h3>
+                                
+                                <p class="product_price_1">
+                                    <?= htmlspecialchars(formatPrice((float)$productData['price']), ENT_QUOTES, 'UTF-8') ?> ₽
+                                </p>
+                            </a>
+                        </article>
+                    </li>
+                <?php } ?>
+            </ul>
+        </section>
+    <?php } ?>
 </div>
