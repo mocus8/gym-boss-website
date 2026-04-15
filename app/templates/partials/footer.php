c8defe6d4a7b54a3c947c56b248eb1e4a07ad456
mocus8 <mocus8@gmail.com>
2026-04-15 22:45:17 +0300

    feat: добавлены стили для футера

--- app/templates/partials/footer.php
+++ app/templates/partials/footer.php
@@ -1,61 +1,67 @@
-<footer class="footer">
-    <div class="footer_disklamer">
-        Внимание: Этот сайт создан для демонстрации навыков разработки и не осуществляет реальную коммерческую деятельность.
+<footer class="container">
+    <div class="site-footer__inner full-size shape-cut-corners">
+        <div class="site-footer__section site-footer-disclaimer">
+            Внимание: Этот сайт создан для демонстрации навыков разработки и не осуществляет реальную коммерческую деятельность.
 
-        <a class="colour_href" href="/about">О сайте</a>
+            <span class="site-footer__section-row">
+                <a href="/about">О сайте</a>
 
-        <a class="colour_href" href="/privacy">Политика конфиденциальности</a>
-    </div>
+                <a href="/privacy">Политика конфиденциальности</a>
+            </span>
+        </div>
 
-    <div class="footer_mail">
-        По всем вопросам писать на почту: <a class="colour_href" href='mailto:mocus8@gmail.com'>mocus8@gmail.com</a>
-    </div>
+        <div class="site-footer__section">
+            <p>По всем вопросам писать на почту:</p>
 
-    <div class="footer_social">
-        <p>Наши соцсети:</p>
+            <a href='mailto:mocus8@gmail.com'>mocus8@gmail.com</a>
+        </div>
 
-        <ul>
-            <li>
-                <a
-                    href="https://vk.com/mocus8"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img class="footer_social_img" src="/assets/images/ui/vk.png" alt="Мы ВКонтакте">
-                </a>
-            </li>
+        <div class="site-footer__section">
+            <p>Наши соцсети:</p>
 
-            <li>
-                <a
-                    href="https://t.me/mocus8"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img class="footer_social_img" src="/assets/images/ui/tg.png" alt="Наш Telegram">
-                </a>
-            </li>
-        </ul>
-    </div>
+            <ul class="list-reset site-footer__section-row">
+                <li>
+                    <a
+                        href="https://vk.com/mocus8"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img class="site-footer__icon" src="/assets/images/ui/vk.png" alt="Мы ВКонтакте">
+                    </a>
+                </li>
 
-    <div class="footer_pay">
-        <p>Способы оплаты:</p>
+                <li>
+                    <a
+                        href="https://t.me/mocus8"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img class="site-footer__icon" src="/assets/images/ui/tg.png" alt="Наш Telegram">
+                    </a>
+                </li>
+            </ul>
+        </div>
 
-        <ul>
-            <li>
-                <img class="footer_pay_img" src="/assets/images/ui/mir.png" alt="МИР">
-            </li>
+        <div class="site-footer__section">
+            <p>Способы оплаты:</p>
 
-            <li>
-                <img class="footer_pay_img" src="/assets/images/ui/visa.png" alt="Visa">
-            </li>
+            <ul class="list-reset site-footer__section-row">
+                <li>
+                    <img class="site-footer__icon" src="/assets/images/ui/mir.png" alt="МИР">
+                </li>
 
-            <li>
-                <img class="footer_pay_img" src="/assets/images/ui/master_card.png" alt="Mastercard">
-            </li>
+                <li>
+                    <img class="site-footer__icon" src="/assets/images/ui/visa.png" alt="Visa">
+                </li>
 
-            <li>
-                <img class="footer_pay_img" src="/assets/images/ui/yoomoney.png" alt="ЮMoney">
-            </li>
-        </ul>
+                <li>
+                    <img class="site-footer__icon" src="/assets/images/ui/master_card.png" alt="Mastercard">
+                </li>
+
+                <li>
+                    <img class="site-footer__icon" src="/assets/images/ui/yoomoney.png" alt="ЮMoney">
+                </li>
+            </ul>
+        </div>
     </div>
 </footer>
