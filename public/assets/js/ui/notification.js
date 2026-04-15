72adedef7846e3dc638fca5cd69e203a65d5519d
mocus8 <mocus8@gmail.com>
2026-04-15 22:24:49 +0300

    refactor: переименовано имя класса на is-open

--- public/assets/js/ui/notification.js
+++ public/assets/js/ui/notification.js
@@ -48,7 +48,7 @@ class Notification {
         this.close();
 
         this.#text.textContent = innerText;
-        this.#notification.classList.add("is-visible");
+        this.#notification.classList.add("is-open");
 
         // Сброс и запуск анимации прогресса
         this.#progress.classList.remove("shrinking");
@@ -65,7 +65,7 @@ class Notification {
     close() {
         if (!this.#notification) return;
 
-        this.#notification.classList.remove("is-visible");
+        this.#notification.classList.remove("is-open");
         this.#progress.classList.remove("shrinking");
 
         if (this.#text) this.#text.textContent = "";
