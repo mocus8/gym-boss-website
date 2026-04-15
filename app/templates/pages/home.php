61f185d9fbc4c58a54a3c8f0e6f38d230e528df1
mocus8 <mocus8@gmail.com>
2026-04-15 10:23:18 +0300

    feat: добавлены стили для содержимого страницы home

--- app/templates/pages/home.php
+++ app/templates/pages/home.php
@@ -1,31 +1,33 @@
 <!-- Контент главной страницы -->
 
-<div class="catalog">
+<div class="container catalog">
     <?php foreach ($catalog as $categoryData) { ?>
-        <section class="category_row">
-            <h2 class="category_name">
+        <section class="catalog__section">
+            <h2 class="catalog__section-title shape-cut-corners">
                 <?= htmlspecialchars($categoryData['category']['name'], ENT_QUOTES, 'UTF-8') ?>
             </h2>
             
-            <ul class="products_main">
+            <ul class="list-reset catalog__section-list">
                 <?php foreach ($categoryData['products'] as $productData) { ?>
-                    <li>
-                        <article>
-                            <a class="product" href="/products/<?= htmlspecialchars($productData['slug'], ENT_QUOTES, 'UTF-8') ?>">
-                                <img class="product_img_1"
+                    <li class="catalog__section-item">
+                        <a
+                            class="link-shell full-size"
+                            href="/products/<?= htmlspecialchars($productData['slug'], ENT_QUOTES, 'UTF-8') ?>"
+                        >
+                            <div class="product-card shape-cut-corners">
+                                <img
+                                    class="img-full shape-cut-corners"
                                     src="<?= htmlspecialchars($productData['image_path'], ENT_QUOTES, 'UTF-8') ?>"
                                     alt="<?= htmlspecialchars($productData['name'], ENT_QUOTES, 'UTF-8') ?>"
                                 >
 
-                                <h3 class="product_name_1">
-                                    <?= htmlspecialchars($productData['name'], ENT_QUOTES, 'UTF-8') ?>
-                                </h3>
+                                <h3><?= htmlspecialchars($productData['name'], ENT_QUOTES, 'UTF-8') ?></h3>
                                 
-                                <p class="product_price_1">
+                                <p class="product-card__price">
                                     <?= htmlspecialchars(formatPrice((float)$productData['price']), ENT_QUOTES, 'UTF-8') ?> ₽
                                 </p>
-                            </a>
-                        </article>
+                            </div>
+                        </a>
                     </li>
                 <?php } ?>
             </ul>
