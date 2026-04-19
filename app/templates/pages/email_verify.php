f7dddb253c4a669d22f25270f200d69f0df7d983
mocus8 <mocus8@gmail.com>
2026-04-19 13:21:40 +0300

    refactor: стиль для формы-карточки вынесен в form.css

--- app/templates/pages/email_verify.php
+++ app/templates/pages/email_verify.php
@@ -7,15 +7,15 @@
         </span>
     </a>
 
-    <div class="email-verify shape-cut-corners">
-        <h1 class="email-verify__title">
+    <div class="form-card shape-cut-corners">
+        <h1 class="form-card__title">
             <?= htmlspecialchars($pageData['title'], ENT_QUOTES, 'UTF-8') ?>
         </h1>
 
         <p> <?= htmlspecialchars($pageData['message'], ENT_QUOTES, 'UTF-8') ?>  </p>
 
         <?php if ($isAuthenticated && $pageData['show_resend']) { ?>  
-            <button id="resend-button" class="btn-reset email-verify__resend-btn btn-shell" type="button">
+            <button id="resend-button" class="btn-reset resend-btn btn-shell" type="button">
                 <span class="btn shape-cut-corners">
                     Получить новое письмо
                 </span>
