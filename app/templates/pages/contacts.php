27e327c903d3f239790c5c5f0a0f44bcc521780d
mocus8 <mocus8@gmail.com>
2026-04-23 18:45:51 +0300

    feat: добавлены стили для техничиских и информационных страницы, немного обновлен README файл

--- app/templates/pages/contacts.php
+++ app/templates/pages/contacts.php
@@ -1,41 +1,24 @@
-<div class="button_return_position">
-    <a href="/">
-        <div class="button_return">
-            <div class="button_return_text">
-                На главную
-            </div>
-            <img class="button_return_img" src="/assets/images/ui/arrow_back.png">
-        </div>
-    </a>
-</div>
-<div class="cart_in_cart_text">
-    Наши контакты:
-</div>
-<div class="contacts">
-    <div class="contact">
-        <div class="contact_type">
-            Телефон:
-        </div>
-        <div class="contact_inf">
-            <a href='tel: +79000000000' class="colour_href">+7 900 000 00 00</a> - по вопросам заказов и работе магазинов<br>
-            <a href='tel: +79000000000' class="colour_href">+7 900 000 00 00</a> - по вопросам о наличии товаров    
-        </div>
-    </div>
-    <div class="contact">
-        <div class="contact_type">
-            Электронная почта:
-        </div>
-        <div class="contact_inf">
-            <a href='mailto:mocus8@gmail.com' class="colour_href">mocus8@gmail.com</a> - по всем вопросам<br>
-        </div>
-    </div>
-    <div class="contact">
-        <div class="contact_type">
-            Социальные сети:
-        </div>
-        <div class="contact_inf">
-            <a href="https://t.me/mocus8" target="_blank" rel="noopener noreferrer" class="colour_href">Telegram разработчика</a><br>
-            <a href="https://vk.com/mocus8" target="_blank" rel="noopener noreferrer" class="colour_href">ВК профиль разработчика</a>
-        </div>
-    </div>
+ <div class="container content-card">
+    <h1 class="page-title">Наши контакты</h1>
+
+    <h2>Телефон</h2>
+
+    <ul class="flex-stack-row shape-cut-corners--diagonal">
+        <li><a href='tel:+79000000000'>+7 900 000 00 00</a> - по вопросам заказов и работе магазинов</li>
+
+        <li><a href='tel:+79000000000'>+7 900 000 00 00</a> - по вопросам о наличии товаров </li>
+    </ul>
+
+    <h2>Электронная почта</h2>
+
+    <p class="flex-stack-row shape-cut-corners--diagonal">
+        <a href='mailto:mocus8@gmail.com'>mocus8@gmail.com</a> - по всем вопросам
+    </p>
+    
+    <h2>Мессенджеры</h2>
+
+    <p class="flex-stack-row shape-cut-corners--diagonal">
+        <a href="https://api.whatsapp.com/send?phone=79167413418" target="_blank" rel="noopener noreferrer">WhatsApp</a>,
+        <a href="https://t.me/mocus8" target="_blank" rel="noopener noreferrer">Telegram (@mocus8)</a>.
+    </p>
 </div>
